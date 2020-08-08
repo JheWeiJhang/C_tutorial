881b4b701b4c80d95c811a319ea36ba513a9402e
JheWei <c123456789q@gmail.com>
2020-08-08 13:01:31 +0800

    [Purpose]Upload the lesson 3

---
+++ Lesson_3/lesson_3.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+ 
+int main(void) {
+	char single_symbol = 'a';
+	char string_array[] = "string array";
+	char *string_pointer = "example string for char pointer"
+	
+	//print the contnet in the variable
+	printf("single_symbol = %c \n", single_symbol);
+	printf("string_array = %s \n", string_array);
+	printf("string_pointer = %s \n", string_array);
+	
+	return 0;
+}
